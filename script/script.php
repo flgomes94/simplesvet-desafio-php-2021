9feb6a4aaf7538d21532c77d2187a01991d8e80d
fabio <fabiolopes.dev@gmail.com>
2021-06-13 19:26:56 -0300

    Criada Parte de Cliente

--- script/script.php
+++ script/script.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "root";
 $database = "complicadovet";
 $colunasAnimal = array('Id','IdCliente','Nome','Raca','Especie','HistoricoClinico','Nascimento');
+$colunasCliente = array('Id','Nome','Telefone1','Telefone2','Email');
 
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
@@ -23,5 +24,23 @@ if($result = $conn->query($queryAnimal)){
     }
     $result->free();
 }
+fclose($arquivoAnimal);
+
+$queryCliente =  "SELECT * from Cliente";
+$arquivoCliente = fopen('Cliente.csv', 'w');
+
+fputcsv($arquivoCliente,$colunasCliente);
+if($result = $conn->query($queryCliente)){
+    while($row = $result->fetch_array()){
+        $linha = array();
+        foreach($colunasCliente as $field){
+            array_push($linha, $row[$field]?? "");
+        }
+        fputcsv($arquivoCliente, $linha);
+    }
+    $result->free();
+}
+fclose($arquivoCliente);
+
 $conn->close();
 ?>
