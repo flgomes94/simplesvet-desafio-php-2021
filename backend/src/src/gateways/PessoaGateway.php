7667d674c03fc19d2d1073fd84d13306ab3ac178
fabio <fabiolopes.dev@gmail.com>
2021-06-14 02:25:24 -0300

    Commit inicial

---
+++ backend/src/src/gateways/PessoaGateway.php
@@ -0,0 +1,30 @@
+<?php
+namespace Src\gateways;
+
+class PessoaGateway {
+
+    private $db = null;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function findAll()
+    {
+        $statement = "
+            SELECT 
+                id, nome
+            FROM
+                pessoas;
+        ";
+
+        try {
+            $statement = $this->db->query($statement);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+}
