9b54ecc69ebe2e395ca0c2372e22359133e5b878
fabio <fabiolopes.dev@gmail.com>
2021-06-14 15:23:22 -0300

    Criada Parte de Insercao de Animais

--- backend/src/src/public/index.php
+++ backend/src/src/public/index.php
@@ -1,10 +1,11 @@
 <?php
 require "../../bootstrap.php";
 
-use Src\controllers\ContatoController;
-use Src\controllers\PessoaController;
+use Src\gateways\AnimalGateway;
 use Src\gateways\ContatoGateway;
+use Src\gateways\EspecieGateway;
 use Src\gateways\PessoaGateway;
+use Src\gateways\RacaGateway;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -34,6 +35,33 @@ if($uri[1] == 'upload'){
             $gatewayContato->insert(array("pessoa_id" => $pessoaInserida['id'], "tipo" =>"fixo","contato"=>$telefone1));
             $gatewayContato->insert(array("pessoa_id" => $pessoaInserida['id'], "tipo" =>"fixo","contato"=>$telefone2));
             $gatewayContato->insert(array("pessoa_id" => $pessoaInserida['id'], "tipo" =>"email","contato"=>$email));
+        }
+        fclose($h);
+    }
+    if (($h = fopen($fileAnimais['tmp_name'], "r")) !== FALSE) {
+        $header = fgetcsv($h, 0, ",");
+        while (($data = fgetcsv($h, 0, ",")) !== FALSE){		
+            $id = $data[0];
+            $idCliente = $data[1];
+            $nome = $data[2];
+            $raca = $data[3];
+            $especie = $data[4];
+            $historico = $data[5];
+            $nascimento = $data[6];
+            $gatewayEspecie = new EspecieGateway($dbConnection);
+            $existeEspecie = $gatewayEspecie->procurarPorNome($especie);
+            if(!$existeEspecie){
+                $existeEspecie = $gatewayEspecie->insert(array("nome" => $especie));
+            }
+            $gatewayRaca = new RacaGateway($dbConnection);
+            $existeRaca = $gatewayRaca->procurarPorNome($raca);
+            if(!$existeRaca){
+                $existeRaca = $gatewayRaca->insert(array("nome" => $raca, "especie_id" => $existeEspecie['id']));
+            }
+            $gatewayAnimal = new AnimalGateway($dbConnection);
+            $gatewayAnimal->insert(array("pessoa_id" => $idCliente,"especie_id" => $existeEspecie['id'],"raca_id" => $existeRaca['id'], "nome"=> $nome,  "nascimento"=> $nascimento));
+
+
         }
         fclose($h);
     }
